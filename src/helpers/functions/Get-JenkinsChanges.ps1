f5dadd9ce3489fcccc79d25578126330b08436f0
Eric Hayes <eric@inflection.com>
2012-11-30 14:58:25 -0800

    Added jenkins changes and upped the version to 1.0.1

--- src/helpers/functions/Get-JenkinsChanges.ps1
+++ src/helpers/functions/Get-JenkinsChanges.ps1
@@ -1,13 +1,17 @@
 function Get-JenkinsChanges {
 param(
-  [Parameter(Mandatory=$true)][string]$buildUrl
+  [string]$buildUrl=""
 )
     $DebugPreference = 'continue'
     $ErrorActionPreference = "Stop"
     
     # parse from the url
-    $xml = [xml](New-Object System.Net.WebClient).DownloadString"$($buildUrl)/api/xml")
-    $message = $xml.freeStyleBuild.changeSet.Item | %{ "$($_.msg) - $($_.user)" }
+    if( $buildUrl -eq "" ) {
+        $message = "NO CHANGES"
+    } else {
+        $xml = [xml](New-Object System.Net.WebClient).DownloadString("$($buildUrl)api/xml")
+        $message = $xml.freeStyleBuild.changeSet.Item | %{ "$($_.msg) - $($_.user)" }
+    }
     
     return $message
 }
